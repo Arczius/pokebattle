873722b25e981cb7ebf379bfe53bb24a9d2cc257
Arczius <70692175+Arczius@users.noreply.github.com>
2022-10-13 21:22:23 +0200

    built a Name class

---
+++ Classes/pokemon/Name.ts
@@ -0,0 +1,18 @@
+"use strict";
+
+export default class Name {
+
+    constructor(
+        private Name:string,
+    ){
+        this.Name = Name;
+    }
+
+    protected setName(Name:string){
+        this.Name = Name;
+    }
+
+    public getName(){
+        return this.Name;
+    }
+}
